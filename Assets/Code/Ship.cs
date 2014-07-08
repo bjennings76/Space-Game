b9ba90d0ff1b06dd4ec54974010bd42ad764327c
Brian Dean Jennings <bjennings@fablelabs.com>
2014-07-07 23:03:58 -0700

    Created sprites and a 'Sprite' folder / Moved code into a 'Code' folder / Turnable ship / Engine flame

---
+++ Assets/Code/Ship.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float Speed = 1f;
+    public float TurnSpeed = 1f;
+    public Engine Engine;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Moving the ship
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MovesShipY(Speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MovesShipY(-Speed);
+        }
+        else
+        {
+            Engine.StopEngine();
+        }
+
+        // Turning the ship
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            TurnShip(-TurnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            TurnShip(TurnSpeed);
+        }
+    }
+
+    // This turns the ship on the Z axis
+    private void TurnShip(float turnSpeed)
+    {
+        transform.Rotate(0, 0, turnSpeed);
+    }
+
+    // This moves the ship in the local Y direction.
+    private void MovesShipY(float speed)
+    {
+        transform.Translate(new Vector3(0f, speed, 0f) * Time.deltaTime);
+        Engine.StartEngine();
+    }
+}
---
+++ Assets/Engine.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class Engine : MonoBehaviour
+{
+    public bool EngineOn;
+    public bool Toggle;
+
+    public void StartEngine()
+    {
+        EngineOn = true;
+        gameObject.SetActive(true);
+    }
+
+    public void StopEngine()
+    {
+        EngineOn = false;
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!EngineOn)
+        {
+            return;
+        }
+
+        Toggle = !Toggle;
+        transform.localScale = Toggle ? new Vector3(1, 1) : new Vector3(0.5f, 0.5f);
+    }
+}
