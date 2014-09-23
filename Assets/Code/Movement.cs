fbb13bde3a98693cae622113d019c5f72ad0fbe3
Franklin Webber <franklin.webber@gmail.com>
2014-09-23 13:28:32 -0700

    Added Asteroids and changed movement to give the example one some spin

--- Assets/Code/Movement.cs
+++ Assets/Code/Movement.cs
@@ -5,7 +5,8 @@ public class Movement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	rigidbody2D.AddForce(-Vector2.right, ForceMode2D.Impulse);
+		rigidbody2D.AddForceAtPosition (-Vector2.right * 5, new Vector2 (1, 1));
+		rigidbody2D.AddForce(-Vector2.right, ForceMode2D.Impulse);
 	}
 	
 	// Update is called once per frame
